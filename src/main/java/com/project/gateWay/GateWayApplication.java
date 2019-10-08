f6fde830d8300d691fe225da807a52914fc72123
yufeng.lin <yufeng.lin@ucarinc.com>
2019-10-08 21:16:00 +0800

    提交git ignore文件

---
+++ src/main/java/com/project/gateWay/GateWayApplication.java
@@ -0,0 +1,13 @@
+package com.project.gateWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+
+}
